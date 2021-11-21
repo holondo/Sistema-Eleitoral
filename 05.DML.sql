e5034faa7f4c0c25620ea7a1474d558f77a97755
LucasBetoni <lucasbetoni@gmail.com>
2021-11-21 15:55:14 -0300

    Update 05.DML.sql

--- 05.DML.sql
+++ 05.DML.sql
@@ -45,6 +45,7 @@ INSERT INTO doacao_pj VALUES('000001', 1, 111.14);
 INSERT INTO pleito VALUES(1, 1110);
 
 INSERT INTO participante_equipe_apoio VALUES(DEFAULT, '11113', 1, 2019);
+----------------------------------------------------temp div-----------------------------------
 
 --cidades e estados chave para exemplificação
 --BRASIL id:1
@@ -57,30 +58,125 @@ INSERT INTO participante_equipe_apoio VALUES(DEFAULT, '11113', 1, 2019);
 
 -- processo_judicial (default,CPF,status_procedente,data_julgamento)
 
-
 -- partido (cod_partido,nome)
+INSERT INTO partido VALUES(19,'Partido Trabalhista Nacional');
+INSERT INTO partido VALUES(50,'Partido Socialismo E Liberdade');
+INSERT INTO partido VALUES(15,'Partido Do Movimento Democrático Brasileiro');
+INSERT INTO partido VALUES(45,'Partido Da Social Democracia Brasileira');
+INSERT INTO partido VALUES(14,'Partido Trabalhista Brasileiro');
+INSERT INTO partido VALUES(36,'Partido Trabalhista Cristão');
+INSERT INTO partido VALUES(23,'Partido Popular Socialista');
+INSERT INTO partido VALUES(54,'Partido Pátria Livre');
+INSERT INTO partido VALUES(11,'Partido Progressista');
+INSERT INTO partido VALUES(43,'Partido Verde');
+INSERT INTO partido VALUES(10,'Partido Republicano Brasileiro');
+INSERT INTO partido VALUES(22,'Partido Da República');
+INSERT INTO partido VALUES(65,'Partido Comunista Do Brasil');
+INSERT INTO partido VALUES(44,'Partido Republicano Progressista');
+INSERT INTO partido VALUES(70,'Partido Trabalhista Do Brasil');
+INSERT INTO partido VALUES(25,'Democratas');
+INSERT INTO partido VALUES(20,'Partido Social Cristão');
+INSERT INTO partido VALUES(13,'Partido Dos Trabalhadores');
+INSERT INTO partido VALUES(12,'Partido Democrático Trabalhista');
+INSERT INTO partido VALUES(27,'Partido Social Democrata Cristão');
+INSERT INTO partido VALUES(17,'Partido Social Liberal');
+INSERT INTO partido VALUES(40,'Partido Socialista Brasileiro');
+INSERT INTO partido VALUES(31,'Partido Humanista Da Solidariedade');
+INSERT INTO partido VALUES(77,'Solidariedade');
+INSERT INTO partido VALUES(55,'Partido Social Democrático');
+INSERT INTO partido VALUES(90,'Partido Republicano Da Ordem Social');
+INSERT INTO partido VALUES(33,'Partido Da Mobilização Nacional');
+INSERT INTO partido VALUES(51,'Partido Ecológico Nacional');
+INSERT INTO partido VALUES(28,'Partido Renovador Trabalhista Brasileiro');
+INSERT INTO partido VALUES(16,'Partido Socialista Dos Trabalhadores Unificado');
+INSERT INTO partido VALUES(21,'Partido Comunista Brasileiro');
+INSERT INTO partido VALUES(29,'Partido Da Causa Operária');
+INSERT INTO partido VALUES(35,'Partido Da Mulher Brasileira');
+INSERT INTO partido VALUES(18,'Rede Sustentabilidade');
+INSERT INTO partido VALUES(30,'Partido Novo');
+INSERT INTO partido VALUES(80,'Unidade Popular');
 
 -- candidato (CPF,partido(int))
 
 -- programa_partido (cod_partido(int),descricao)
-
+INSERT INTO programa_partido VALUES(19,'PODE :  Partido Trabalhista Nacional :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(50,'PSOL :  Partido Socialismo E Liberdade :  ');
+INSERT INTO programa_partido VALUES(15,'PMDB :  Partido Do Movimento Democrático Brasileiro :  Aliança Por Um Acre Melhor');
+INSERT INTO programa_partido VALUES(45,'PSDB :  Partido Da Social Democracia Brasileira :  Aliança Por Um Acre Melhor');
+INSERT INTO programa_partido VALUES(14,'PTB :  Partido Trabalhista Brasileiro :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(36,'PTC :  Partido Trabalhista Cristão :  Aliança Por Um Acre Melhor I - Federal');
+INSERT INTO programa_partido VALUES(23,'PPS :  Partido Popular Socialista :  Aliança Por Um Acre Melhor Iii');
+INSERT INTO programa_partido VALUES(54,'PPL :  Partido Pátria Livre :  Frente Trabalhista Republicana Humanista Livre');
+INSERT INTO programa_partido VALUES(11,'PP :  Partido Progressista :  Aliança Por Um Acre Melhor');
+INSERT INTO programa_partido VALUES(43,'PV :  Partido Verde :  Produzir Para Empregar');
+INSERT INTO programa_partido VALUES(10,'PRB :  Partido Republicano Brasileiro :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(22,'PR :  Partido Da República :  Aliança Por Um Acre Melhor');
+INSERT INTO programa_partido VALUES(65,'PC do B :  Partido Comunista Do Brasil :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(44,'PRP :  Partido Republicano Progressista :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(70,'AVANTE :  Partido Trabalhista Do Brasil :  Aliança Por Um Acre Melhor Ii');
+INSERT INTO programa_partido VALUES(25,'DEM :  Democratas :  Produzir Para Empregar');
+INSERT INTO programa_partido VALUES(20,'PSC :  Partido Social Cristão :  Aliança Por Um Acre Melhor');
+INSERT INTO programa_partido VALUES(13,'PT :  Partido Dos Trabalhadores :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(12,'PDT :  Partido Democrático Trabalhista :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(27,'PSDC :  Partido Social Democrata Cristão :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(17,'PSL :  Partido Social Liberal :  Frente Popular Do Acre Ii');
+INSERT INTO programa_partido VALUES(40,'PSB :  Partido Socialista Brasileiro :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(31,'PHS :  Partido Humanista Da Solidariedade :  Frente Popular Do Acre');
+INSERT INTO programa_partido VALUES(77,'SOLIDARIEDADE :  Solidariedade :  Aliança Por Um Acre Melhor I - Federal');
+INSERT INTO programa_partido VALUES(55,'PSD :  Partido Social Democrático :  Aliança Por Um Acre Melhor');
+INSERT INTO programa_partido VALUES(90,'PROS :  Partido Republicano Da Ordem Social :  Frente Popular Do Acre Ii');
+INSERT INTO programa_partido VALUES(33,'PMN :  Partido Da Mobilização Nacional :  Produzir Para Empregar');
+INSERT INTO programa_partido VALUES(51,'PATRIOTA :  Partido Ecológico Nacional :  Frente Popular Do Acre 5');
+INSERT INTO programa_partido VALUES(28,'PRTB :  Partido Renovador Trabalhista Brasileiro :  O Futuro Começa Agora');
+INSERT INTO programa_partido VALUES(16,'PSTU :  Partido Socialista Dos Trabalhadores Unificado :  ');
+INSERT INTO programa_partido VALUES(21,'PCB :  Partido Comunista Brasileiro :  ');
+INSERT INTO programa_partido VALUES(29,'PCO :  Partido Da Causa Operária :  ');
+INSERT INTO programa_partido VALUES(35,'PMB :  Partido Da Mulher Brasileira :  Por Um Novo Amazonas');
+INSERT INTO programa_partido VALUES(18,'REDE :  Rede Sustentabilidade :  O Começo De Uma Grande Mudança');
+INSERT INTO programa_partido VALUES(30,'NOVO :  Partido Novo :  ');
+INSERT INTO programa_partido VALUES(80,'UP :  Unidade Popular :  ');
 
 --individuo_juridico (CNPJ,nome)
-
---CREATE DOMAIN tipo_localidade CHECK(VALUE in ('cidade', 'estado', 'federacao'));
-
-
---CHECK (VALUE IN ('RJ','SP','ES','MG','PR','SC','RS','MS','GO','AC','AL','AP','AM','BA','CE','DF','MA','MT','PA','PB','PE','PI','RN','RO','RR','SE','TO'));
 
 INSERT INTO federacao VALUES(1, 'Brasil');
 
 INSERT INTO estado VALUES(1, 'SP', 'Sao Paulo');
 INSERT INTO estado VALUES(1, 'MG', 'Minas Gerais');
 INSERT INTO estado VALUES(1, 'RJ', 'Rio de Janeiro');
+INSERT INTO estado VALUES(1, 'AC','Acre');
+INSERT INTO estado VALUES(1, 'AL','Alagoas');
+INSERT INTO estado VALUES(1, 'AM','Amazonas');
+INSERT INTO estado VALUES(1, 'AP','Amapá');
+INSERT INTO estado VALUES(1, 'BA','Bahia');
+INSERT INTO estado VALUES(1, 'CE','Ceará');
+INSERT INTO estado VALUES(1, 'DF','Distrito Federal');
+INSERT INTO estado VALUES(1, 'ES','Espírito Santo');
+INSERT INTO estado VALUES(1, 'GO','Goiás');
+INSERT INTO estado VALUES(1, 'MA','Maranhão');
+INSERT INTO estado VALUES(1, 'MG','Minas Gerais');
+INSERT INTO estado VALUES(1, 'MS','Mato Grosso do Sul');
+INSERT INTO estado VALUES(1, 'MT','Mato Grosso');
+INSERT INTO estado VALUES(1, 'PA','Pará');
+INSERT INTO estado VALUES(1, 'PB','Paraíba');
+INSERT INTO estado VALUES(1, 'PE','Pernambuco');
+INSERT INTO estado VALUES(1, 'PI','Piauí');
+INSERT INTO estado VALUES(1, 'PR','Paraná');
+INSERT INTO estado VALUES(1, 'RJ','Rio de Janeiro');
+INSERT INTO estado VALUES(1, 'RN','Rio Grande do Norte');
+INSERT INTO estado VALUES(1, 'RO','Rondônia');
+INSERT INTO estado VALUES(1, 'RR','Roraima');
+INSERT INTO estado VALUES(1, 'RS','Rio Grande do Sul');
+INSERT INTO estado VALUES(1, 'SC','Santa Catarina');
+INSERT INTO estado VALUES(1, 'SE','Sergipe');
+INSERT INTO estado VALUES(1, 'SP','São Paulo');
+INSERT INTO estado VALUES(1, 'TO','Tocantins');
 
-INSERT INTO cidade VALUES(1, 'Americana','SP');
-INSERT INTO cidade VALUES(1, 'Valinhos','MG');
-INSERT INTO cidade VALUES(1, 'SBO', 'SP');
+INSERT INTO cidade VALUES(1, 'Campinas','SP');
+INSERT INTO cidade VALUES(1, 'São Carlos','SP');
+INSERT INTO cidade VALUES(1, 'São Paulo', 'SP');
+INSERT INTO cidade VALUES(1, 'Curitiba', 'PR');
+
+
 --cargo (nome,localidade,quant_eleitos)
 
 -- CANDIDATURA (default,nome_cargo,localidade,CPF_candidato,ano,vice)
